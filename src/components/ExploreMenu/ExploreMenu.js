d1ab9fc926eecf0c722468fb8fc870e9df070dee
Banukaiw <banukaiw@gmail.com>
2024-06-30 23:05:54 +0530

    version 02

--- src/components/ExploreMenu/ExploreMenu.js
+++ src/components/ExploreMenu/ExploreMenu.js
@@ -2,21 +2,22 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/frontend_assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore Our Menu</h1>
-        <p className='explore-menu-text'>uuuuuuuuuuu uuuuuu u u u u u uuuuuuuuuuuuuuu u u u uu u uu  u u uuuuuu u </p>
+        <p className='explore-menu-text'>fuuuuuuufguuuuck uuuuuu u u u u u uuuuuuuuuuuuuuu u u u uu u uu  u u uuuuuu u </p>
         <div className='explore-menu-list'>
             {menu_list.map((item,index)=>{
                 return (
-                    <div key={index} className='explore-menu-list-item'>
-                        <img src={item.menu_image} alt=""></img>
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=""></img>
                         <p>{item.menu_name}</p>
                     </div>
                 )
             })}
         </div>
+        <hr/>
     </div>
   )
 }
