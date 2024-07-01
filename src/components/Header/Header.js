d09e58b4a46db9cdf82c9a7a878c08d9653a44bf
Banukaiw <banukaiw@gmail.com>
2024-07-01 23:27:04 +0530

    version 04

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='container'>
         <div className='header-contents'>
             <h2>Order your favoutite food head</h2> 
-            <p>hfffffffffffffffffff ffffffffffffffffffffffff fffffffffffffffff hhhhhhh hhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhhhh hhhhhhhhh hhhhh</p>
+            <p>Welcome to Taste Wish, where we bring delicious meals from your favorite restaurants straight to your doorstep, ensuring a convenient, quick, and delightful dining experience with just a few clicks. Savor the convenience of gourmet dining at home.</p>
             <button>View Menu</button>
         </div>
         </div>
