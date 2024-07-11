67b6273fccfb7c5051df467b5312d46ac5b33ad7
Banukaiw <banukaiw@gmail.com>
2024-07-11 20:15:47 +0530

    version 06

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
+import { useState } from 'react';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 //import Header from './components/Header/Header';
 
 
 function App() {
+
+  const [showLogin, setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup/>:<></>}
     <div className='app'>
 
-    <Navbar/>
+    <Navbar setShowLogin = {setShowLogin}/>
     
     <Routes>
       <Route path="/" element={<Home/>}></Route>
