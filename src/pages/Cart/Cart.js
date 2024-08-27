6ebd7ddefb07df53a2510cdbbe06d3eb585cefef
Banukaiw <banukaiw@gmail.com>
2024-08-27 22:27:03 +0530

    Version 09

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import {StoreContext} from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
-  const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext)
+  const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div className='cart-item'>
@@ -57,8 +59,9 @@ const Cart = () => {
               <p>RS.{getTotalCartAmount()+2}</p>
             </div>
             
+            
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className='cart-promocode'>
           <p>If you have a promo code, Enter it hear</p>
--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import './PlaceOrder.css'
-
+import React from "react";
+import "./PlaceOrder.css";
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
-  )
-}
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivert Information</p>
+        <div className="muilti-fileds">
+          <input type="text" placeholder="first name"></input>
+          <input type="text" placeholder="last name"></input>
+        </div>
+        <input type="text" placeholder="Email Adress"></input>
+        <input type="text" placeholder="Street"></input>
+        <div className="muilti-fileds">
+          <input type="text" placeholder="City"></input>
+          <input type="text" placeholder="State"></input>
+        </div>
+        <div className="muilti-fileds">
+          <input type="text" placeholder="Zip Code"></input>
+          <input type="text" placeholder="Counrty"></input>
+        </div>
+        <input type="text" placeholder="phone"></input>
+      </div>
+      <div className="place-order-right"></div>
+    </form>
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
